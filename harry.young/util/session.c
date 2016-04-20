dd67f01caaef3c0b00c6fbc09e26f1668f32b67a
Tony <harry.a.young@gmail.com>
2016-04-20 18:55:53 -0400

    File Issues
    Fixing file issues from initial commit

--- harry.young/util/session.c
+++
@@ -1,8 +0,0 @@
-#include "session.h"
-
-void free_session(SESSION** session){
-    if(*session != NULL){
-        free(*session);
-        *session = NULL;
-    }
-}
--- harry.young/util/user.c
+++
@@ -1,6 +0,0 @@
-#include "user.h"
-
-void free_user(USER** user){
-    free(*user);
-    *user = NULL;
-}
